a6c34628982e606d2c95ed425aba6ae29d62f354
Chinmaya Andukuri <andukuri.stanford.edu>
2023-12-23 03:37:55 -0600

    add function perturbations

--- utils.py
+++ utils.py
@@ -4,9 +4,127 @@ import random
 
 class Perturber:
     def __init__(self):
+        self.expr_mutator = ExprMutator()
+        self.var_mutator = VariableMutator()
+        self.func_mutator = FunctionMutator()
+
+    # High Level Helpers
+    def randomly_modify_code(self, code):
         """
-        Initialize the Perturber class with a dictionary of mutation functions.
+        Randomly modify Python code by mutating expressions or replacing variables.
+
+        args:
+            code: str
+                The Python code to modify.
+
+        return:
+            output: str
+                The modified Python code.
         """
+        tree = ast.parse(code)
+        parsed_tree = ast.walk(tree)
+
+
+        var_nodes = self.var_mutator.collect_var_nodes(parsed_tree)
+        expr_nodes = self.expr_mutator.collect_expr_nodes(parsed_tree)
+
+
+        if random.choice([True, False]):
+            modified_tree = self.var_mutator.replace_variable(tree, var_nodes)
+        else:
+            modified_tree = self.expr_mutator.adjust_expr(tree, expr_nodes)
+
+
+        return ast.unparse(modified_tree)
+
+
+    def randomly_modify_expression(self, code):
+        """
+        Randomly modify Python code by mutating an expression, if possible.
+
+        args:
+            code: str
+                The Python code to modify.
+
+        return:
+            output: str
+                The modified Python code.
+        """
+        tree = ast.parse(code)
+        parsed_tree = ast.walk(tree)
+        expr_nodes = self.expr_mutator.collect_expr_nodes(parsed_tree)
+
+        if len(expr_nodes) == 0: return code
+        modified_tree = self.expr_mutator.adjust_expr(tree, expr_nodes)
+        return ast.unparse(modified_tree)
+
+
+    def randomly_modify_variable(self, code):
+        """
+        Randomly modify Python code by mutating a variable, if possible.
+
+        args:
+            code: str
+                The Python code to modify.
+
+        return:
+            output: str
+                The modified Python code.
+        """
+        tree = ast.parse(code)
+        parsed_tree = ast.walk(tree)
+        var_nodes = self.var_mutator.collect_var_nodes(parsed_tree)
+        if len(var_nodes) < 2: return code
+
+        modified_tree = self.var_mutator.replace_variable(tree, var_nodes)
+        return ast.unparse(modified_tree)
+
+
+    def randomly_modify_functioncall(self, code):
+        """
+        Randomly modify Python code by replacing a function call, if possible.
+
+        args:
+            code: str
+                The Python code to modify.
+
+        return:
+            output: str
+                The modified Python code.
+        """
+        tree = ast.parse(code)
+        parsed_tree = ast.walk(tree)
+        func_call_nodes = self.func_mutator.collect_func_call_nodes(parsed_tree)
+
+        if len(func_call_nodes) < 2:
+            return code
+
+        modified_tree = self.func_mutator.replace_function_call(tree, func_call_nodes)
+        return ast.unparse(modified_tree)
+
+
+    def same_tree(self, a, b):
+        """
+        Check if two Python functions have the same Abstract Syntax Tree.
+
+        args:
+            a: str
+                The first Python function in string format.
+            b: str
+                The second Python function in string format.
+
+        return:
+            output: bool
+                True if the functions have the same tree, False otherwise.
+        """
+        return ast.dump(ast.parse(a)) == ast.dump(ast.parse(b))
+
+
+
+
+
+class ExprMutator:
+    def __init__(self):
         self.mutation_functions = {
             ast.IfExp: self.mutate_IfExp,
             ast.Lambda: self.mutate_Lambda,
@@ -20,11 +138,6 @@ class Perturber:
         }
 
 
-
-
-
-
-    
     # Operator Perturbations
     def collect_expr_nodes(self, parsed_tree):
         """
@@ -38,12 +151,12 @@ class Perturber:
             output: List
                 The list of all expression nodes in the generator.
         """
-        
+
         implemented_types = self.mutation_functions.keys()
 
         return [node for node in parsed_tree if isinstance(node, tuple(implemented_types))]
         # Eventually, we will have implemented mutations for all expressions.
-        # For now, since we only have a few implemented, it is inefficient 
+        # For now, since we only have a few implemented, it is inefficient
         #   to try to sample from those we have not implemented yet and get a new
         #   corrupted piece of code very infrequently
         #return [node for node in parsed_tree if isinstance(node, ast.expr) or isinstance(node, ast.stmt)]
@@ -221,6 +334,9 @@ class Perturber:
 
 
 
+class VariableMutator:
+    def __init__(self):
+        pass
 
     def collect_var_nodes(self, parsed_tree):
         """
@@ -234,7 +350,16 @@ class Perturber:
             output: List
                 The list of all variable nodes in the generator.
         """
-        return [node for node in parsed_tree if isinstance(node, ast.Name) and isinstance(node.ctx, ast.Load)]
+        var_nodes = []
+        for node in parsed_tree:
+            # Check if the node is a function definition
+            if isinstance(node, ast.FunctionDef):
+                # Collect variable nodes from the function body
+                for body_node in ast.walk(node):
+                    if isinstance(body_node, ast.Name) and isinstance(body_node.ctx, ast.Load):
+                        var_nodes.append(body_node)
+
+        return var_nodes
 
 
     def replace_variable(self, tree: ast.AST, var_nodes):
@@ -252,103 +377,66 @@ class Perturber:
         # Choose a node to replace and remove it from the list of potential replacements
         node_to_replace = random.choice(var_nodes)
         potential_replacements = [node for node in var_nodes if node.id != node_to_replace.id]
+
         # Guarantee a different choice of node for the replacement
         if len(potential_replacements) == 0: return tree
         replacement_node = random.choice(potential_replacements)
         node_to_replace.id = replacement_node.id
         return tree
 
-    
 
 
 
 
+class FunctionMutator:
+    def __init__(self):
+        pass
 
-    # High Level Helpers
-    def randomly_modify_code(self, code):
+    def collect_func_call_nodes(self, parsed_tree):
         """
-        Randomly modify Python code by mutating expressions or replacing variables.
+        Collect all function call nodes within the function body.
 
         args:
-            code: str
-                The Python code to modify.
+            parsed_tree: generator
+                The generator to search for function calls.
 
         return:
-            output: str
-                The modified Python code.
+            output: List
+                The list of all function call nodes in the generator.
         """
-        tree = ast.parse(code)
-        parsed_tree = ast.walk(tree)
-        
+        func_call_nodes = []
+        for node in parsed_tree:
+            if isinstance(node, ast.FunctionDef):
+                for body_node in ast.walk(node):
+                    if isinstance(body_node, ast.Call):
+                        func_call_nodes.append(body_node)
+        return func_call_nodes
 
-        var_nodes = self.collect_var_nodes(parsed_tree)
-        expr_nodes = self.collect_expr_nodes(parsed_tree)
-
-
-        if random.choice([True, False]):
-            modified_tree = self.replace_variable(tree, var_nodes)
-        else:
-            modified_tree = self.adjust_expr(tree, expr_nodes)
-        
-
-        return ast.unparse(modified_tree)
-
-
-    def randomly_modify_expression(self, code):
+    def replace_function_call(self, tree: ast.AST, func_call_nodes):
         """
-        Randomly modify Python code by mutating an expression, if possible.
+        Replace one function call with another in the AST.
 
         args:
-            code: str
-                The Python code to modify.
+            tree: ast.AST
+                The abstract syntax tree in which to replace function calls.
 
         return:
-            output: str
-                The modified Python code.
+            output: ast.AST
+                The abstract syntax tree with replaced function calls.
         """
-        tree = ast.parse(code)
-        parsed_tree = ast.walk(tree)
-        expr_nodes = self.collect_expr_nodes(parsed_tree)
+        if len(func_call_nodes) < 2:
+            return tree
 
-        if len(expr_nodes) == 0: return code
-        modified_tree = self.adjust_expr(tree, expr_nodes)
-        return ast.unparse(modified_tree)
-    
+        # Choose a node to replace and remove it from the list of potential replacements
+        node_to_replace = random.choice(func_call_nodes)
+        potential_replacements = [node for node in func_call_nodes if node.func != node_to_replace.func]
 
-    def randomly_modify_variable(self, code):
-        """
-        Randomly modify Python code by mutating a variable, if possible.
+        # Ensure there's a different choice for replacement
+        if not potential_replacements:
+            return tree
 
-        args:
-            code: str
-                The Python code to modify.
+        replacement_node = random.choice(potential_replacements)
 
-        return:
-            output: str
-                The modified Python code.
-        """
-        tree = ast.parse(code)
-        parsed_tree = ast.walk(tree)
-        var_nodes = self.collect_var_nodes(parsed_tree)
-        if len(var_nodes) < 2: return code
-
-        modified_tree = self.replace_variable(tree, var_nodes)
-        return ast.unparse(modified_tree)
-
-
-        
-    def same_tree(self, a, b):
-        """
-        Check if two Python functions have the same Abstract Syntax Tree.
-
-        args:
-            a: str
-                The first Python function in string format.
-            b: str
-                The second Python function in string format.
-
-        return:
-            output: bool
-                True if the functions have the same tree, False otherwise.
-        """
-        return ast.dump(ast.parse(a)) == ast.dump(ast.parse(b))
+        # Replace the function call
+        node_to_replace.func = replacement_node.func
+        return tree
